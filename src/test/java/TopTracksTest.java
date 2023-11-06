6c79d8f39fb52493b6d28bc005a85d3f53838a27
theultrafirefox <kanakaeff.s@gmail.com>
2023-11-06 23:13:53 +0500

    actualised test "test name"

--- src/test/java/TopTracksTest.java
+++ src/test/java/TopTracksTest.java
@@ -18,6 +18,6 @@ public class TopTracksTest {
     @Test
     void getTopTrackTest() {
         assertEquals("My Love Mine All Mine", musicTopTracks.getTopTracks().get(0).getName());
-        assertEquals("Cruel Summer", musicTopTracks.getTopTracks().get(1).getName());
+        assertEquals("Style (Taylor's Version)", musicTopTracks.getTopTracks().get(1).getName());
     }
 }
