7bbdfffb94d4c7aa0a1a81279dce63df151f2cbd
StAl15 <stihinaleksander@yandex.ru>
2023-12-01 01:13:55 +0500

    add command table pattern

--- src/main/java/org/example/bot/command/Command.java
+++ src/main/java/org/example/bot/command/Command.java
@@ -1,14 +1,7 @@
 package org.example.bot.command;
 
-public enum Command {
-    START,
-    HELP,
-    ABOUT,
-    GET_SINGERS,
-    GET_POPULAR,
-    UNKNOWN,
-    ADD_SONG,
-    SHARE_PLAYLIST,
-    GET_PLAYLIST,
-    CREATE_PLAYLIST
+public interface Command {
+    void execute();
+
+    public String name();
 }
---
+++ src/main/java/org/example/bot/command/CommandEnum.java
@@ -0,0 +1,15 @@
+package org.example.bot.command;
+
+public enum CommandEnum {
+    START,
+    HELP,
+    ABOUT,
+    GET_SINGERS,
+    GET_POPULAR,
+    UNKNOWN,
+    ADD_SONG,
+    SHARE_PLAYLIST,
+    GET_PLAYLIST,
+    GET_MY_PLAYLISTS,
+    CREATE_PLAYLIST
+}
