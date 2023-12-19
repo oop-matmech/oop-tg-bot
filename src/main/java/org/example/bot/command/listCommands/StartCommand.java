b67cafa55c7c4e105f26a63fd18d87b357769b12
theultrafirefox <kanakaeff.s@gmail.com>
2023-12-19 13:37:44 +0500

    минорные исправления глобального обновления  (v.0.4)

---
+++ src/main/java/org/example/bot/command/listCommands/StartCommand.java
@@ -0,0 +1,35 @@
+package org.example.bot.command.listCommands;
+
+import org.example.bot.Bot;
+import org.example.bot.command.Command;
+import org.example.bot.command.CommunicatorWrapper;
+import org.example.bot.communicator.ICommunicator;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+public class StartCommand extends CommunicatorWrapper implements Command {
+    public StartCommand(ICommunicator communicator, Message message, Bot bot) {
+        super(communicator, message, bot);
+    }
+
+    public StartCommand() {
+    }
+
+    @Override
+    public void execute() {
+        communicator.sendText(
+                bot,
+                message.getFrom().getId(),
+                """
+                        Добро пожаловать
+                        Команды:
+                        /help - памагите
+                        /about - хто я
+                        """.trim()
+        );
+    }
+
+    @Override
+    public String name() {
+        return "start";
+    }
+}
